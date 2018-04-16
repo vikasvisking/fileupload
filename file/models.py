5787b10dba181a014dcd89a2c422aa0628d373dd
vikasvisking <36699373+vikasvisking@users.noreply.github.com>
2018-04-17 00:28:30 +0530

    it a file uploading project in django
    it only uploads the file and image to the server.
    
    also i am trying to display all the file that have being uploaded on my webpage.
    
    anybody please help me in it

--- file/models.py
+++ file/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 # Create your models here.
 class File(models.Model):
-    file = models.ImageField(upload_to='documents', blank=True)
+    file = models.FileField(upload_to='documents', blank=True)
 
     def __str__(self):
         return self.file.name
